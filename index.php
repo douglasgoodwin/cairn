5e6459e9f463a5a1788a29d5bfda85515e5610f3
Douglas Goodwin <dgoodwin@gmail.com>
2021-06-30 10:00:39 -0700

    specify some routes

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 
 require __DIR__ . '/kirby/bootstrap.php';
 
+$kirby = new Kirby([
+    'roots' => [
+        'index'   => __DIR__,
+        'content' => __DIR__ . '/../content',
+        'site'    => __DIR__ . '/../site',
+    ],
+]);
+
 echo (new Kirby)->render();
